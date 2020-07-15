cbdfda7f012c5c08c28456acc7d1cc39105965f2
amaurote <acey.music@gmail.com>
2020-07-16 01:15:22 +0200

    update

--- NewOpenGL/src/Application.cpp
+++ NewOpenGL/src/Application.cpp
@@ -3,93 +3,13 @@
 
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <sstream>
 
 #include "Renderer.h"
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "VertexBuffer.h"
-
-struct ShaderProgram
-{
-	std::string VertexSource;
-	std::string FragmentSource;
-};
-
-static ShaderProgram ParseShader(const std::string& filepath)
-{
-	std::ifstream stream(filepath);
-
-	enum class ShaderType
-	{
-		NONE = -1,
-		VERTEX = 0,
-		FRAGMENT = 1
-	};
-
-	std::string line;
-	std::stringstream ss[2];
-	ShaderType type = ShaderType::NONE;
-	while (getline(stream, line))
-	{
-		if (line.find("#shader") != std::string::npos)
-		{
-			if (line.find("vertex") != std::string::npos)
-				type = ShaderType::VERTEX;
-			else if (line.find("fragment") != std::string::npos)
-				type = ShaderType::FRAGMENT;
-		}
-		else if (type != ShaderType::NONE)
-		{
-			ss[static_cast<int>(type)] << line << '\n';
-		}
-	}
-
-	return {ss[0].str(), ss[1].str()};
-}
-
-static unsigned int CompileShader(unsigned int type, const std::string& source)
-{
-	unsigned int id = glCreateShader(type);
-	const char* src = source.c_str();
-	glShaderSource(id, 1, &src, nullptr);
-	glCompileShader(id);
-
-	int result;
-	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-	if (result == GL_FALSE)
-	{
-		int length;
-		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-		char* message = (char*)_malloca(length * sizeof(char));
-		glGetShaderInfoLog(id, length, &length, message);
-		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader" <<
-			std::endl;
-		std::cout << message << std::endl;
-		glDeleteShader(id);
-		return 0;
-	}
-
-	return id;
-}
-
-static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
-{
-	unsigned int program = glCreateProgram();
-	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
-
-	glAttachShader(program, vs);
-	glAttachShader(program, fs);
-	glLinkProgram(program);
-	glValidateProgram(program);
-
-	glDeleteShader(vs);
-	glDeleteShader(fs);
-
-	return program;
-}
+#include "Shader.h"
+#include "VertexBufferLayout.h"
 
 int main(void)
 {
@@ -145,60 +65,54 @@ int main(void)
 
 		IndexBuffer ib(indices, 6);
 
-		ShaderProgram source = ParseShader("res/shaders/basic.shader");
-		unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-		glUseProgram(shader);
-
-		// uniform
-		int location = glGetUniformLocation(shader, "u_Color");
-		ASSERT(location != -1);
-		glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f);
-
+		Shader shader("res/shaders/basic.shader");
+		shader.Bind();
+		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+		
 		va.Unbind();
-		glUseProgram(0);
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		shader.Unbind();
+		vb.Unbind();
+		ib.Unbind();
+
+		Renderer renderer;
 
 		// animation
 		float r = 0.0f;
 		float g = 0.3f;
 		float b = 0.8f;
-		float increment_r = 0.05;
-		float increment_g = 0.05;
-		float increment_b = 0.05;
+		float increment_r = 0.05f;
+		float increment_g = 0.05f;
+		float increment_b = 0.05f;
 
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window))
 		{
 			/* Render here */
-			glClear(GL_COLOR_BUFFER_BIT);
+			renderer.Clear();
 
-			glUseProgram(shader);
-			glUniform4f(location, r, g, b, 1.0f);
+			shader.Bind();
+			shader.SetUniform4f("u_Color", r, g, b, 1.0f);
 
-			va.Bind();
-			ib.Bind();
-			
-			GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+			renderer.Draw(va, ib, shader);
 
 			if (r > 1.0f)
-				increment_r = -0.02;
+				increment_r = -0.02f;
 			else if (r < 0)
-				increment_r = 0.01;
+				increment_r = 0.01f;
 
 			r += increment_r;
 
 			if (g > 1.0f)
-				increment_g = -0.03;
+				increment_g = -0.03f;
 			else if (g < 0)
-				increment_g = 0.01;
+				increment_g = 0.01f;
 
 			g += increment_g;
 
 			if (b > 1.0f)
-				increment_b = -0.02;
+				increment_b = -0.02f;
 			else if (b < 0)
-				increment_b = 0.03;
+				increment_b = 0.03f;
 
 			b += increment_b;
 
@@ -208,7 +122,6 @@ int main(void)
 			/* Poll for and process events */
 			glfwPollEvents();
 		}
-		glDeleteProgram(shader);
 	}
 
 	glfwTerminate();
--- NewOpenGL/src/VertexBufferLayout.h
+++ NewOpenGL/src/VertexBufferLayout.h
@@ -16,9 +16,8 @@ struct VertexBufferElement
 			case GL_FLOAT: return 4;
 			case GL_UNSIGNED_INT: return 4;
 			case GL_UNSIGNED_BYTE: return 1;
+			default: ASSERT(false); return 0;
 		}
-		ASSERT(false);
-		return 0;
 	}
 };
 
