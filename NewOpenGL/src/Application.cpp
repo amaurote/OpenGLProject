901c93e7d657e7799783420e7c12861e5e962739
amaurote <acey.music@gmail.com>
2020-07-17 23:56:39 +0200

    update

--- NewOpenGL/src/Application.cpp
+++ NewOpenGL/src/Application.cpp
@@ -10,6 +10,14 @@
 #include "VertexBuffer.h"
 #include "Shader.h"
 #include "VertexBufferLayout.h"
+#include "Texture.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui_impl_glfw.h"
 
 int main(void)
 {
@@ -18,10 +26,6 @@ int main(void)
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
-
-	// glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	// glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
 	std::cout << "[INFO] GLFW Version:\t" << glfwGetVersionString() << std::endl;
 
@@ -45,10 +49,10 @@ int main(void)
 
 	{
 		float positions[] = {
-			-0.5f, -0.5f,
-			0.5f, -0.5f,
-			0.5f, 0.5f,
-			-0.5f, 0.5f
+			-0.5f, -0.5f, 0.0f, 0.0f,
+			 0.5f, -0.5f, 1.0f, 0.0f,
+			 0.5f,  0.5f, 1.0f, 1.0f,
+			-0.5f,  0.5f, 0.0f, 1.0f
 		};
 
 		unsigned int indices[] = {
@@ -56,26 +60,44 @@ int main(void)
 			2, 3, 0
 		};
 
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+		GLCall(glEnable(GL_BLEND));
+		
 		VertexArray va;
-		VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
 		VertexBufferLayout layout;
+		layout.Push<float>(2);
 		layout.Push<float>(2);
 		va.AddBuffer(vb, layout);
 
 		IndexBuffer ib(indices, 6);
 
+		glm::mat proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+		
+
 		Shader shader("res/shaders/basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+		shader.SetUniformMat4f("u_MVP", proj);
+
+		Texture texture("res/textures/woof.png");
+		texture.Bind();
+		shader.SetUniform1i("u_Texture", 0);
 		
 		va.Unbind();
-		shader.Unbind();
 		vb.Unbind();
 		ib.Unbind();
+		shader.Unbind();
 
 		Renderer renderer;
 
+		// imgui
+		ImGui::CreateContext();
+		ImGui::StyleColorsDark();
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		ImGui_ImplOpenGL3_Init((char*)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
+		
 		// animation
 		float r = 0.0f;
 		float g = 0.3f;
@@ -84,12 +106,19 @@ int main(void)
 		float increment_g = 0.05f;
 		float increment_b = 0.05f;
 
+		bool show_demo_window = true;
+		bool show_another_window = false;
+		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+		
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window))
 		{
 			/* Render here */
 			renderer.Clear();
 
+			//New Frame
+			ImGui_ImplOpenGL3_NewFrame();
+			
 			shader.Bind();
 			shader.SetUniform4f("u_Color", r, g, b, 1.0f);
 
@@ -116,6 +145,32 @@ int main(void)
 
 			b += increment_b;
 
+
+			{
+				static float f = 0.0f;
+				static int counter = 0;
+
+				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+				ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+				ImGui::Checkbox("Another Window", &show_another_window);
+
+				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+				if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+					counter++;
+				ImGui::SameLine();
+				ImGui::Text("counter = %d", counter);
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+				ImGui::End();
+			}
+
+			
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+			
 			/* Swap front and back buffers */
 			glfwSwapBuffers(window);
 
@@ -123,6 +178,12 @@ int main(void)
 			glfwPollEvents();
 		}
 	}
+
+	//Shutdown
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+
+	ImGui::DestroyContext();
 
 	glfwTerminate();
 	return 0;
--- NewOpenGL/src/Shader.h
+++ NewOpenGL/src/Shader.h
@@ -2,6 +2,8 @@
 #include <string>
 #include <unordered_map>
 
+#include "glm/glm.hpp"
+
 struct ShaderProgram
 {
 	std::string VertexSource;
@@ -17,17 +19,21 @@ public:
 	void Bind() const;
 	void Unbind() const;
 
+	// set uniforms
+	void SetUniform1i(const std::string& name, int value);
+	void SetUniform1f(const std::string& name, float value);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+	void SetUniformMat4f(const std::string& name, const glm::mat4& matrix);
 	
 private:
 	unsigned int m_RendererID;
 	std::string m_FilePath;
 	std::unordered_map<std::string, int> m_UniformLocationCache;
 	
-	unsigned int GetUniformLocation(const std::string name);
 	ShaderProgram ParseShader(const std::string& filepath);
 	unsigned int CompileShader(unsigned int type, const std::string& source);
 	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
-	
+
+	int GetUniformLocation(const std::string& name);
 };
 
